e776c9fbf1c0748cba1d313b5b98c35311e65cf4
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-01-12 00:51:36 -0800

    updates

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
 alert("This Website and Painting, took a lot of hours 🕦 to code and draw, a hopfully, it does James McBride well! - Hadrian Lazic 👍");
+
+const nodes = document.querySelectorAll('.node');
+
+const hoverSound = new Audio('assets/hoversound.mp3');
+
+nodes.forEach(node => {
+    node.addEventListener('mouseenter', () => {
+        hoverSound.currentTime = 0; 
+        hoverSound.play();
+    });
+});
